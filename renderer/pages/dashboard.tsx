10575bd8edfeb57c1f736d82da34f24d61107a50
tamagram <tamadevelopment211@gmail.com>
2022-07-06 13:48:41 +0900

    :sparkles: Add dashboard page

---
+++ renderer/pages/dashboard.tsx
@@ -0,0 +1,11 @@
+import Layout from "../components/Layout";
+const DashboardPage = () => {
+  return (
+    <Layout title="Dashboard">
+      <h1>This is dashboard</h1>
+      <p>text here</p>
+    </Layout>
+  );
+};
+
+export default DashboardPage;
