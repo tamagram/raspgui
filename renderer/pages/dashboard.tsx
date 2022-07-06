9564e6cfea08dc3bf569f883853ffae5dcbec70f
tamagram <tamadevelopment211@gmail.com>
2022-07-07 05:35:18 +0900

    :sparkles: Update UI

--- renderer/pages/dashboard.tsx
+++ renderer/pages/dashboard.tsx
@@ -1,10 +1,54 @@
 import Layout from "../components/Layout";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const labels = ["January", "February", "March", "April", "May", "June", "July"];
+
+const data = {
+  labels,
+  datasets: [
+    {
+      label: "Dataset 1",
+      data: labels.map(() => Math.random() * 100),
+      borderColor: "rgb(255, 99, 132)",
+      backgroundColor: "rgba(255, 99, 132, 0.5)",
+    },
+    {
+      label: "Dataset 2",
+      data: labels.map(() => Math.random() * 100),
+      borderColor: "rgb(53, 162, 235)",
+      backgroundColor: "rgba(53, 162, 235, 0.5)",
+    },
+  ],
+};
 
 const DashboardPage = () => {
   return (
     <Layout title="Dashboard">
       <h1 className="text-5xl underline">This is dashboard</h1>
       <p>text here</p>
+      <div className="flex flex-col bg-white border shadow-sm rounded-xl p-4 md:p-5 dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-700/[.7] dark:text-gray-400">
+        <Line data={data} width={100} height={50} />
+      </div>
     </Layout>
   );
 };
