40b88907cc4f6231fc958647c6bb5dadf7ba70d6
tamagram <tamadevelopment211@gmail.com>
2022-07-07 04:32:29 +0900

    :sparkles: Update UI

--- renderer/components/Layout.tsx
+++ renderer/components/Layout.tsx
@@ -9,30 +9,36 @@ type Props = {
 };
 
 const Layout = ({ children, title = "This is the default title" }: Props) => (
-  <div>
+  <div className="dark:bg-slate-900 flex flex-col min-h-screen">
     <Head>
       <title>{title}</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
-    <header>
-      <nav>
-        <Link href="/">
-          <a>Home</a>
-        </Link>{" "}
-        |{" "}
-        <Link href="/about">
-          <a>About</a>
-        </Link>{" "}
-        |{" "}
-        <Link href="/initial-props">
-          <a>With Initial Props</a>
-        </Link>
+    <header className="flex flex-wrap sm:justify-start sm:flex-nowrap z-50 w-full bg-white text-sm py-4 dark:bg-gray-800">
+      <nav className="max-w-[85rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between">
+        <a className="flex-none text-xl font-semibold dark:text-white" href="#">
+          RaspGUI
+        </a>
+        <div className="flex flex-row items-center gap-5 mt-5 sm:justify-end sm:mt-0 sm:pl-5">
+          <Link href="/">
+            <a className="flex-none text-xl font-semibold dark">Home</a>
+          </Link>
+          <Link href="/dashboard">
+            <a>Dashboard</a>
+          </Link>
+          <Link href="/about">
+            <a>About</a>
+          </Link>
+          <Link href="/initial-props">
+            <a>With Initial Props</a>
+          </Link>
+        </div>
       </nav>
     </header>
-    {children}
-    <footer>
-      <hr />
+    <main className="flex flex-col px-4 py-4">{children}</main>
+    <footer className="mt-auto px-4 py-3">
+      <hr className="py-1" />
       <span>I&apos;m here to stay (Footer)</span>
     </footer>
   </div>
