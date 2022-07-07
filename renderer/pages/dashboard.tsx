aef16d495ac4163f1477522efa3cea468f34f91b
tamagram <tamadevelopment211@gmail.com>
2022-07-08 01:37:06 +0900

    :rotating_light: Fix linter warnings

--- renderer/pages/dashboard.tsx
+++ renderer/pages/dashboard.tsx
@@ -8,10 +8,11 @@ import {
   Title,
   Tooltip,
   Legend,
+  ChartOptions,
 } from "chart.js";
 import { Line } from "react-chartjs-2";
 import ChartStreaming from "chartjs-plugin-streaming";
-import 'chartjs-adapter-luxon';
+import "chartjs-adapter-luxon";
 
 ChartJS.register(
   CategoryScale,
@@ -44,29 +45,29 @@ const data = {
   ],
 };
 
-const options = {
+const options: ChartOptions = {
   plugins: {
     streaming: {
-      duration: 20000
-    }
+      duration: 20000,
+    },
   },
   scales: {
     x: {
-      type: 'realtime',
+      type: "realtime",
       realtime: {
         duration: 20000,
-onRefresh: chart => {
-            chart.data.datasets.forEach(dataset => {
-              dataset.data.push({
-                x: Date.now(),
-                y: Math.random() * 100
-              });
+        onRefresh: (chart) => {
+          chart.data.datasets.forEach((dataset) => {
+            dataset.data.push({
+              x: Date.now(),
+              y: Math.random() * 100,
             });
-          }
-      }
-    }
-  }
-}
+          });
+        },
+      },
+    },
+  },
+};
 
 const DashboardPage = () => {
   return (
@@ -74,7 +75,7 @@ const DashboardPage = () => {
       <h1 className="text-5xl underline">This is dashboard</h1>
       <p>text here</p>
       <div className="flex flex-col bg-white border shadow-sm rounded-xl p-4 md:p-5 dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-700/[.7] dark:text-gray-400">
-        <Line data={data} width={100} height={50} options={options}/>
+        <Line data={data} width={100} height={50} options={options} />
       </div>
     </Layout>
   );
